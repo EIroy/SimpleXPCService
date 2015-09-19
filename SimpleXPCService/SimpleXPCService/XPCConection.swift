e933c24937c36ebd8fa923a18a2b27c9c7f3f367
Admin <admin@Mac-Admin.local>
2015-09-19 23:35:34 +0300

    removed not related to project data

--- SimpleXPCService/SimpleXPCService/XPCConection.swift
+++ SimpleXPCService/SimpleXPCService/XPCConection.swift
@@ -26,9 +26,3 @@ class SimpleConnectionController: NSObject {
         self.connection.invalidate()
     }
 }
-echo # SimpleXPCService >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/EIroy/SimpleXPCService.git
-git push -u origin master
